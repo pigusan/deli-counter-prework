bf671996b91eeab9090b329c0ae8f75cfd408b32
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:18:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = ["Bob", "Bill", "Brendan"]
 
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     message = "The line is currently"
     katz_deli.each_with_index do | person, index|
