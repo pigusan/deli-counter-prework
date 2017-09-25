d4f284a6b3123bdd624b37f3eeeb7727d34924df
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:19:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    message = "The line is currently"
+    message = "The line is currently: "
     katz_deli.each_with_index do | person, index|
       message += "#{index + 1}. #{person}"
     end
