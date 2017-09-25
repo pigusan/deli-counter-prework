f1550da566782400ccce8ea03b090500c2f4c6ab
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:15:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+katz_deli = ["Bob", "Bill", "Brendan"]
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else 
+    katz_deli.each_with_index do | person, index|
+      puts ""
